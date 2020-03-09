4f1adc88d73e1969f074e4ff2ed2de9e5504154a
Atsushi-Kuwabara <48851734+Atsushi-Kuwabara@users.noreply.github.com>
2020-03-09 15:23:33 +0900

    自動デプロイエラー修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,8 @@ set :repo_url,  'git@github.com:Atsushi-Kuwabara/task-matrix.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
