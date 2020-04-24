04d0c52cc5ca9a867cc7c4de62f8f51ea4a5130c
Atsushi-Kuwabara <48851734+Atsushi-Kuwabara@users.noreply.github.com>
2020-04-24 17:10:46 +0900

    並び替え機能実装

--- app/assets/javascripts/task_sort.js
+++ app/assets/javascripts/task_sort.js
@@ -1,6 +1,20 @@
 $(function() {
-  $('#sort_order').change(function(){
+  $(document).on('change', '#sort_order', function(){
     var this_value = $(this).val();
-    console.log(this_value)
+    if (this_value == "importance_asc") {
+      html = "tasks?&sort=importance_id+asc"
+    } else if (this_value == "urgency_asc") {
+      html = "tasks?&sort=urgency_id+asc"
+    } else {
+      html = ""
+    };
+    var current_html = window.location.href;
+    
+    //要リファクタリング。クリックごURLが残らないようにする必要あり
+    //if (location['href'].match(/tasks?&sort=*.+/) != null) {
+    //  var remove_html = location['href'].match(/tasks?&sort=*.+/)
+    //  var current_html = current_html.replace(remove_html, '')
+    //};
+    window.location.href = current_html + html
   });
 });
